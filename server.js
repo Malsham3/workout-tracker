63326a9c95aed164822ad13160f80cbb49f6d9ba
Mostafa <m.alsham627@gmail.com>
2021-01-04 22:00:20 -0700

    logger + htmlRoutes

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const PORT = process.env.PORT || 3000;
 //Sets up the express app to handle data parsing
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(logger("dev"));
 
 //Make express look up the files relative to the public directory
 app.use(express.static('public'));
 
-//Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app)
+//Routes\
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 
 //connect
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
